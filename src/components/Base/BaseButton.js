5d8ff8ac4876cb810a9dc7c70067982876e24219
Valera Shabalin <shabalinvalera.ts@gmail.com>
2021-06-02 14:41:52 +0700

    Update NotFoundPage

--- src/components/Base/BaseButton.js
+++ src/components/Base/BaseButton.js
@@ -1,18 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 class BaseButton extends React.Component {
+    handleClick(event) {
+        event.preventDefault();
+        alert('Hi!');
+    }
+
     render() {
         return (
-            <a href="#" className="base-button">
-                {this.props.title}
+            <a
+                className="base-button"
+                onClick={() => this.handleClick}
+            >
+                {this.props.children}
             </a>
         );
     }
 }
-
-BaseButton.propTypes = {
-    title: PropTypes.string.isRequired
-};
 
 export default BaseButton;
